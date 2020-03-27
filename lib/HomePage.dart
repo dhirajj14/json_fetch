f8e7e96056de5ebe30bed6944631f7d5aac984ce
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-03-27 14:58:40 -0500

    add card from fetch data

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -3,6 +3,8 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
+import 'model/Data.dart';
+
 
 class Home extends StatefulWidget {
   @override
@@ -10,6 +12,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+
+  Future<List<Data>> getAllData() async{
+
+    var api="https://jsonplaceholder.typicode.com/photos";
+    var data=await http.get(api);
+
+    var jsonData = json.decode(data.body);
+
+    List<Data>listof=[];
+
+    for(var i in jsonData){
+
+      Data data = new Data(i["id"], i["title"], i["url"], i["thumbnailUrl"]);
+      listof.add(data);
+
+    }
+
+    return listof;
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -87,6 +112,48 @@ class _HomeState extends State<Home> {
             ),
           ],
         ),
+      ),
+
+      body: new ListView(
+        children: <Widget>[
+
+          new Container(
+            margin: EdgeInsets.all(10.0),
+            height: 250.0,
+            child: new FutureBuilder(
+                future: getAllData(),
+                builder: (BuildContext c, AsyncSnapshot snapshot){
+                  if(snapshot.data == null){
+                    return Center(
+                      child: new Text("Loading Data"),
+                    );
+                  }else{
+                    return ListView.builder(
+                      itemCount: snapshot.data.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext c, int index){
+                        return Card(
+                          elevation: 10.0,
+                          child: new Column(
+                            children: <Widget>[
+
+                              new Image.network(snapshot.data[index].url,
+
+                                height: 150.0,
+                                width: 150.0,
+                                fit: BoxFit.cover,
+                              ),
+
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  }
+              }
+              ),
+            ),
+        ],
       ),
     );
   }
