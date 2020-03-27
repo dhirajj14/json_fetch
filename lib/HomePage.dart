5fb00620c97029a1ab934a11fed73bbe08b81700
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-03-27 12:15:29 -0500

    add data model

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
 
 class Home extends StatefulWidget {
   @override
---
+++ lib/model/Data.dart
@@ -0,0 +1,11 @@
+class Data{
+
+  int id;
+  String title;
+  String url;
+  String thumbnailUrl;
+
+  Data(this.id, this.title, this.url, this.thumbnailUrl);
+
+
+}
